24909d17d9ac05894dff819b72d2d248a908c974
L3x4-4 <furkan.tarhan013@gmail.com>
2026-03-03 23:19:52 +0300

    4. soru çözüldü

---
+++ 696. Count Binary Substrings/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _696.Count_Binary_Substrings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "00110011";
+            Console.WriteLine(CountBinarySubstrings(s)); // 6
+        }
+        static int CountBinarySubstrings(string s)
+        {
+            int prevGroup = 0;
+            int currGroup = 1;
+            int result = 0;
+
+            for (int i = 1; i < s.Length; i++)
+            {
+                if (s[i] == s[i - 1])
+                {
+                    currGroup++;
+                }
+                else
+                {
+                    result += Math.Min(prevGroup, currGroup);
+                    prevGroup = currGroup;
+                    currGroup = 1;
+                }
+            }
+
+            result += Math.Min(prevGroup, currGroup);
+
+            return result;
+        }
+    }
+}
