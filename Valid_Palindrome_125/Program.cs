99919b832b2e72f31c6a8f93c6e8d8b0856af2ef
L3x4-4 <furkan.tarhan013@gmail.com>
2026-03-14 23:58:50 +0300

    5. Soruyu güncelledim

--- Valid_Palindrome_125/Program.cs
+++ Valid_Palindrome_125/Program.cs
@@ -10,41 +10,38 @@ namespace Valid_Palindrome_125
     {
         static void Main(string[] args)
         {
-
-
-
+            Console.WriteLine(IsPalindrome("Küllük"));
 
         }
-        public bool IsPalindrome(string s)
+        public static bool IsPalindrome(string s)
         {
-            // İki işaretçi (Two Pointers) yöntemi
-            int left = 0;
-            int right = s.Length - 1;
+            int left = 0; // şimdi ilk başta imleç hareketini kontrol etmek için harflere numara vereceğiz bunun için baştaki harftan başlamak için sol = 0 
+            int right = s.Length - 1; // sondaki harften başlamak için dizinin son elamanına atıyoruz ama unutmamak gerek ki bizim dizi elamanı sayısı misal 6 ise 
+                                      // s.Lengt =6    right =6 olamaz çünkü dizi elamanları 0 dan başlar yani son elaman 4 olmak zorunda ondan dolayı " -1" olarak elamanı ayarladık
 
-            while (left < right)
+            while (left < right)  // stringi gezmek için while döngüsüne girdik
             {
-                // Soldaki karakter geçerli bir harf veya rakam değilse ilerle
-                if (!char.IsLetterOrDigit(s[left]))
-                {
-                    left++;
+                if (!char.IsLetterOrDigit(s[left])) // şu "char.IsLetterOrDigit " bize karakterin harf mı yoksa rakam mı olduğunu teyit etmek için gerekli bir methot 
+                {                                   // eğer harf ve rakamsa true  diğer semboller ise false döndürür
+                    left++;                         // baştaki "! " dikkat harf rakam değilse karakteri bir yana kaydırıyoruz
                 }
-                // Sağdaki karakter geçerli değilse geri gel
                 else if (!char.IsLetterOrDigit(s[right]))
                 {
-                    right--;
+                    right--;             // aynı işlem burada da tekrar ediyor deilse sağ karakteri bir azalt
                 }
-                // İkisi de geçerliyse karşılaştır
                 else
                 {
-                    if (char.ToLower(s[left]) != char.ToLower(s[right]))
-                    {
-                        return false; // Eşleşmiyorsa palindrome değildir
+                    if (char.ToLower(s[left]) != char.ToLower(s[right]))       // şu "char.ToLower" karekteri küçük harfe çevirmemiz yarar çünkü "Kazak  "bir polindromdur 
+                    {                                                          // ama 'K' ve 'k ' farklı karekter olduğundan onun önüne geçmek için kullandığımız bir methoddur
+                        return false;
                     }
                     left++;
                     right--;
-                }
-            }
 
+                }
+
+
+            }
             return true;
         }
     }
