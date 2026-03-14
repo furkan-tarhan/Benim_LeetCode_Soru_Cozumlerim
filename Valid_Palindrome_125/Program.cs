9c88f1060585dc52a4da4986c0688f5aba9fc725
L3x4-4 <furkan.tarhan013@gmail.com>
2026-03-14 23:27:21 +0300

    5. soru çözüldü

---
+++ Valid_Palindrome_125/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Valid_Palindrome_125
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+
+        }
+        public bool IsPalindrome(string s)
+        {
+            // İki işaretçi (Two Pointers) yöntemi
+            int left = 0;
+            int right = s.Length - 1;
+
+            while (left < right)
+            {
+                // Soldaki karakter geçerli bir harf veya rakam değilse ilerle
+                if (!char.IsLetterOrDigit(s[left]))
+                {
+                    left++;
+                }
+                // Sağdaki karakter geçerli değilse geri gel
+                else if (!char.IsLetterOrDigit(s[right]))
+                {
+                    right--;
+                }
+                // İkisi de geçerliyse karşılaştır
+                else
+                {
+                    if (char.ToLower(s[left]) != char.ToLower(s[right]))
+                    {
+                        return false; // Eşleşmiyorsa palindrome değildir
+                    }
+                    left++;
+                    right--;
+                }
+            }
+
+            return true;
+        }
+    }
+}
