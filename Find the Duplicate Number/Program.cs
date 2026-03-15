2004355165cce71f392f0f1895f07603423e5aee
L3x4-4 <furkan.tarhan013@gmail.com>
2026-03-15 23:10:37 +0300

    6. soru çözüldü

---
+++ Find the Duplicate Number/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Find_the_Duplicate_Number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+
+
+        }
+
+        public int FindDuplicate(int[] nums)
+        {
+            // Önce diziyi küçükten büyüğe sıralıyoruz
+            // {1, 3, 4, 2, 2} -> {1, 2, 2, 3, 4} olur
+            Array.Sort(nums);
+
+            // Diziyi gezip yandaki elemanla aynı mı diye bakıyoruz
+            for (int i = 0; i < nums.Length - 1; i++)
+            {
+                if (nums[i] == nums[i + 1])
+                {
+                    return nums[i]; // Bulduğumuz an döndürüyoruz
+                }
+            }
+
+            return -1; // Normalde buraya hiç gelmez
+        }
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+    }
+}
---
+++ Find the Duplicate Number/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Find the Duplicate Number")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Find the Duplicate Number")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("b387f4b0-d4a0-4f32-bcc7-d098c6a6257e")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
