a68b7015717d234c8fa4c7be64a0e83b1e985b5b
L3x4-4 <furkan.tarhan013@gmail.com>
2026-03-17 23:35:00 +0300

    8. soru güncellendi

--- Best_Time_to_Buy_and_Sell_Stock_LeetCode_121/Program.cs
+++ Best_Time_to_Buy_and_Sell_Stock_LeetCode_121/Program.cs
@@ -10,67 +10,37 @@ namespace Best_Time_to_Buy_and_Sell_Stock_LeetCode_121
     {
         static void Main(string[] args)
         {
-            //    int[] fiyatlar = {1,4,3,1,5,9,7 };
+            int[] fiyatlar = { 1, 4, 3, 1, 5, 9, 7 };  
 
-            //    int sonuc = yatırım(fiyatlar);
+            int sonuc = yatırım(fiyatlar);
 
-            //    Console.WriteLine(sonuc);
-            int[] fiyatlar = { 7, 1, 5, 3, 6, 4 };
-
-            int maxKar = MaxProfit(fiyatlar);
-
-            Console.WriteLine("Elde edilebilecek maksimum kar: " + maxKar);
-            Console.ReadLine();
-
+            Console.WriteLine("Elde edilebilecek max kar: "+sonuc);
+            
         }
-        public static int MaxProfit(int[] prices)
+    
+        public static int yatırım(int[] prices)
         {
-            if (prices.Length == 0) return 0;
+            int minPrice = prices[0];
+            int maxPrice = 0;
+            int kar = 0;
 
-            int minPrice = prices[0]; // İlk günün fiyatını en düşük kabul et
-            int maxProfit = 0;        // Başlangıçta kar sıfır
-
-            for (int i = 1; i < prices.Length; i++)
+            for (int i = 0; i < prices.Length; i++)
             {
-                // Eğer bugünkü fiyat, şu ana kadar gördüğümüzden düşükse en düşüğü güncelle
-                if (prices[i] < minPrice)
+                if (minPrice > prices[i])
                 {
                     minPrice = prices[i];
                 }
-                // Değilse, bugünkü fiyattan satsaydık ne kadar kar ederdik?
-                else if (prices[i] - minPrice > maxProfit)
+                else if (maxPrice < prices[i])
                 {
-                    maxProfit = prices[i] - minPrice;
-                }
+                    maxPrice = prices[i];
+                    
+                } 
             }
 
-            return maxProfit;
+            kar = maxPrice - minPrice;
+
+            return kar;
         }
-        //public static int yatırım(int[] prices)
-        //{
-        //    int minPrice = 0;
-        //    int maxPrice = 0;
-        //    for (int i =0; i<prices.Length;i++)
-        //    {
-
-        //        if (maxPrice < prices[i])
-        //        {
-        //            maxPrice = prices[i];
-        //        }
-        //        else
-        //        {
-        //            minPrice = prices[i];
-        //        }
-
-        //    }
-        //    Console.WriteLine(maxPrice);
-        //    Console.WriteLine(minPrice);
-
-
-
-
-        //    return prices[0];
-        //}
 
 
 
