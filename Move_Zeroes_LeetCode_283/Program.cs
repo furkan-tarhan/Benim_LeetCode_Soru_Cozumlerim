6f4e9a3fea8e6d7b67e0a4f9896a896a8946648b
L3x4-4 <furkan.tarhan013@gmail.com>
2026-03-18 19:40:38 +0300

    9. soru çözüldü

---
+++ Move_Zeroes_LeetCode_283/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Move_Zeroes_LeetCode_283
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            int[] sayilar = { 0, 1, 0, 3, 12 };
+
+            MoveZeroes(sayilar);
+
+            // Sonucu ekrana yazdıralım
+            Console.WriteLine("Sonuç: " + string.Join(", ", sayilar));
+            Console.ReadLine();
+
+
+
+
+
+        }
+        public static void MoveZeroes(int[] nums)
+        {
+            int lastNonZeroFoundAt = 0;
+
+            // 1. Adım: Sıfır olmayanları sırayla en başa diziyoruz
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] != 0)
+                {
+                    nums[lastNonZeroFoundAt] = nums[i];
+                    lastNonZeroFoundAt++;
+                }
+            }
+
+            // 2. Adım: Geri kalan yerleri sıfırla dolduruyoruz
+            for (int i = lastNonZeroFoundAt; i < nums.Length; i++)
+            {
+                nums[i] = 0;
+            }
+        }
+    }
+}
+    
