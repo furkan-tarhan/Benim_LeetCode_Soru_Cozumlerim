be5ac6b3ab5b9655abbe99b334a922c3c91ad247
L3x4-4 <furkan.tarhan013@gmail.com>
2026-03-21 15:54:14 +0300

    10. soru çözüldü

---
+++ Reverse_Words_in_a_String _III_LeetCode_557/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reverse_Words_in_a_String__III_LeetCode_557
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string cumle = "HELLO GUYS ";
+
+            string sonuc = ReserveWords(cumle);
+
+            Console.WriteLine("Orijinal: " + cumle);
+            Console.WriteLine("Ters Hali: " + sonuc);
+            Console.ReadLine();
+
+
+
+
+        }
+
+        public static string ReserveWords(string word)
+        {
+            string[] kelime = word.Split(' ');
+            
+            for (int i =0; i<kelime.Length; i++)
+            {
+                char[] harfler = kelime[i].ToCharArray();
+
+                Array.Reverse(harfler);
+
+                kelime[i] = new string(harfler);
+
+            }
+
+
+
+            return string.Join(" ",kelime);
+
+
+        }
+
+
+
+
+
+
+
+
+
+    }
+}
