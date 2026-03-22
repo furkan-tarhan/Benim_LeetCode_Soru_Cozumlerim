3d1f5467ad246a566fb8338492d363c00e4e852d
L3x4-4 <furkan.tarhan013@gmail.com>
2026-03-22 23:37:36 +0300

    11. soru çözüldü

---
+++ LeetCode 268 Missing Number/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode_268_Missing_Number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] sayilar = { 3, 0, 1 };
+
+            int eksik = MissingNumber(sayilar);
+
+            Console.WriteLine("Dizideki eksik sayı: " + eksik);
+            Console.ReadLine();
+
+        }
+        public static int MissingNumber(int[] nums)
+        {
+            int n = nums.Length;
+
+            // 1. Olması gereken toplam (Gauss Formülü)
+            int olmasiGerekenToplam = n * (n + 1) / 2;
+
+            // 2. Dizideki sayıların gerçek toplamı
+            int gercekToplam = 0;
+            foreach (int sayi in nums)
+            {
+                gercekToplam += sayi;
+            }
+
+            // 3. Aradaki fark eksik sayıdır
+            return olmasiGerekenToplam - gercekToplam;
+        }
+    }
+}
