6c99b86eb118457f0606023c913ac7dd7ee02b9f
L3x4-4 <furkan.tarhan013@gmail.com>
2026-04-02 22:30:08 +0300

    14. Soru eklendi

---
+++ Majority Element/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Majority_Element
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] sayilar = { 2, 2, 1, 1, 1, 2, 2 };
+
+            int sonuc = MajorityElement(sayilar);
+
+            Console.WriteLine("Dizideki çoğunluk elemanı: " + sonuc);
+            Console.ReadLine();
+        }
+        public static int MajorityElement(int[] nums)
+        {
+            // 1. Diziyi küçükten büyüğe sırala
+            Array.Sort(nums);
+
+            // 2. Madem bu sayı yarısından fazla var, 
+            // sıralı dizinin tam ortasındaki eleman kesinlikle odur!
+            return nums[nums.Length / 2];
+        }
+    }
+}
