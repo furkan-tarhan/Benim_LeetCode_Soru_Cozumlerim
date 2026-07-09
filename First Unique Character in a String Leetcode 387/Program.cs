7f480d4a062c028dd44e5d42efb5badc70b5c847
Furkan Tarhan <furkan.tarhan013@gmail.com>
2026-07-09 23:48:24 +0300

    onbeş

--- First Unique Character in a String Leetcode 387/Program.cs
+++ First Unique Character in a String Leetcode 387/Program.cs
@@ -15,6 +15,7 @@ namespace First_Unique_Character_in_a_String_Leetcode_387
             double a = 0.1 + 0.2;
             Console.WriteLine($"{a:F90}");
      Şaşkın sana ne dedim sen ne yaptın , dün gece gördüm seni  essaltu vessalmu aleyke ya resullallah akvaryum şu fıratın suyu serindir akar 
+                daha güz gelmeden dalı bükülmüş hangi fırtınadan kökü sükülmmüş baharda bir başka günler yabancı 
                 ,
 
 
